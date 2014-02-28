a2fa9b1ac012ec93bdbd4ce1c6844ee9d561c562
Martin Yrjölä <martin.yrjola@gmail.com>
2014-02-28 19:15:10 +0200

    More fixes for archlinux

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,24 +8,29 @@ when "ubuntu","debian"
   include_recipe "apt"
   include_recipe "build-essential"
   package "texinfo"
+  include_recipe "subversion"
 when "arch"
-  bash "install base-devel" do
-  user "root"
-  not_if "gcc --version && bison --version"
-  code <<-EOH
+  include_recipe "pacman"
+  bash "update packages" do
+    user "root"
+    code <<-EOH
     pacman -Syu --noconfirm
-    pacman -S --noconfirm ruby ntp base-devel
-    ntpdate -u pool.ntp.org
-    gem install ohai --no-rdoc --no-ri
-    gem install chef --no-rdoc --no-ri
   EOH
+  end
+  pacman_group "base-devel" do
+    action :install
+  end
+  package "fakeroot"
+  chef_gem "digest"
+  include_recipe "git"
+  pacman_aur "omnibus-chef" do
+    action [:build, :install]
   end
 end
 
 include_recipe "users"
-
+include_recipe "openssh"
 include_recipe "git"
-#include_recipe "subversion"
 include_recipe "mercurial"
 package "cvs"
 include_recipe "emacs"
