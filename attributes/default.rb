83822f3476262d2759a650b80fb48d3c1de0e338
Martin Yrjölä <martin.yrjola@gmail.com>
2014-03-09 22:00:51 +0200

    Add postfix ssl paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,3 +7,7 @@ default[:authorization][:sudo][:groups] = ["wheel", "sysadmin"]
 default[:authorization][:sudo][:agent_forwarding] = true
 
 default[:aide][:report_url] = "mailto:root@localhost"
+
+default['postfix']['main']['smtpd_tls_cert_file'] = '/etc/ssl/certs/#{node[:fqdn]}.pem'
+default['postfix']['main']['smtpd_tls_key_file'] = '/etc/ssl/private/#{node[:fqdn]}.key'
+default['postfix']['main']['smtpd_tls_CAfile'] = '/etc/ssl/certs/#{node[:hostname]}-bundle.crt'
