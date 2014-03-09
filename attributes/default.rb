51666e7810c790468ba1cffa8f57230d008ab48a
Martin Yrjölä <martin.yrjola@gmail.com>
2014-03-09 22:09:22 +0200

    Add one more postfix CAfile attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,8 @@ default[:authorization][:sudo][:groups] = ["wheel", "sysadmin"]
 default[:authorization][:sudo][:agent_forwarding] = true
 
 default[:aide][:report_url] = "mailto:root@localhost"
-
 default['postfix']['main']['smtpd_tls_cert_file'] = "/etc/ssl/certs/#{node[:fqdn]}.pem"
 default['postfix']['main']['smtpd_tls_key_file'] = "/etc/ssl/private/#{node[:fqdn]}.key"
-default['postfix']['main']['smtpd_tls_CAfile'] = "/etc/ssl/certs/#{node[:hostname]}-bundle.crt"
+cafile = "/etc/ssl/certs/#{node[:hostname]}-bundle.crt"
+default['postfix']['main']['smtpd_tls_CAfile'] = cafile
+default['postfix']['main']['smtp_tls_CAfile'] = cafile
